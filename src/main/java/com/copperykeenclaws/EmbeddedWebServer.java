60798d3b941adaa5eee4ecc853c66010adb66697
tborthwick <tborthwick@gmail.com>
2011-11-22 20:02:39 -0600

    switched to tomcat

--- src/main/java/com/copperykeenclaws/EmbeddedWebServer.java
+++ src/main/java/com/copperykeenclaws/EmbeddedWebServer.java
@@ -1,32 +1,41 @@
 package com.copperykeenclaws;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppContext;
+import java.io.File;
+
+import org.apache.catalina.core.AprLifecycleListener;
+import org.apache.catalina.core.StandardServer;
+import org.apache.catalina.startup.Tomcat;
 
 public class EmbeddedWebServer {
 
 	private static final long serialVersionUID = -6777759990550225380L;
 
 	public static void main(String[] args) throws Exception{
-        String webappDirLocation = "src/main/webapp/";
+        String appBase = "src/main/webapp/";
+        File appBaseDir = new File(appBase);
         
         String webPort = System.getenv("PORT");
         if(webPort == null || webPort.isEmpty()) {
             webPort = "8080";
         }
         
-        Server server = new Server(Integer.valueOf(webPort));
-        WebAppContext root = new WebAppContext();
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(Integer.parseInt(webPort));
      
-        root.setContextPath("/");
-        root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
-        root.setResourceBase(webappDirLocation);
-     
-        root.setParentLoaderPriority(true);
-     
-        server.setHandler(root);
-     
-        server.start();
-        server.join();
+        tomcat.setBaseDir(".");
+        tomcat.getHost().setAppBase(appBaseDir.getAbsolutePath());
+        
+        // Add AprLifecycleListener
+        StandardServer server = (StandardServer) tomcat.getServer();
+        AprLifecycleListener listener = new AprLifecycleListener();
+        server.addLifecycleListener(listener);
+
+        String contextPath = "/";
+        
+        tomcat.addWebapp(contextPath, appBaseDir.getAbsolutePath());
+        
+        // Start server
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
