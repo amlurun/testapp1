532386b3a1d9df3ccc0a20b9e5891a5297a34967
tborthwick <tborthwick@gmail.com>
2011-11-24 23:14:38 -0600

    jj

--- src/main/java/com/copperykeenclaws/controller/HelloWorldController.java
+++ src/main/java/com/copperykeenclaws/controller/HelloWorldController.java
@@ -3,11 +3,15 @@ package com.copperykeenclaws.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
+@Controller
 public class HelloWorldController extends AbstractController {
 
+	@RequestMapping("/hello")
 	protected ModelAndView handleRequestInternal(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 
